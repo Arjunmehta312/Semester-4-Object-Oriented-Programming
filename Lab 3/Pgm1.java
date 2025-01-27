f6adf93014b685d65dfc9343069c1464caec11a5
Mpstme Student <mpstme.student@SVKMGRP.COM>
2025-01-27 13:42:17 +0530

    Pushed Lab 3 .java files

---
+++ Lab 3/Pgm1.java
@@ -0,0 +1,18 @@
+package Practical3;
+
+/**
+ *
+ * @author Arjun Mehta
+ */
+public class Pgm1 {
+    
+    public static void main(String[] args) {
+        // Loop through numbers 1 to 15
+        for (int i = 1; i <= 15; i++) {
+            // Calculate square of the number
+            int square = i * i;
+            // Print the square
+            System.out.println("Square of " + i + " is: " + square);
+        }
+    }
+}
---
+++ Lab 3/Pgm4.java
@@ -0,0 +1,50 @@
+package Practical3;
+
+/**
+ *
+ * @author Arjun Mehta
+ */
+public class Pgm4 {
+
+    public boolean isPrime(int num) {
+        if (num <= 1) { //
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    public int reverseNumber(int num) {
+        int reversed = 0;
+        while (num != 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        return reversed;
+    }
+
+    public void printEmirps() {
+        int count = 0;
+        int num = 10; 
+
+        while (count < 100) {
+            if (isPrime(num)) {
+                int reversed = reverseNumber(num);
+                if (isPrime(reversed) && num != reversed) {
+                    System.out.print(num + " ");
+                    count++;
+                }
+            }
+            num++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Pgm4 obj = new Pgm4();
+        obj.printEmirps();
+    }
+}
